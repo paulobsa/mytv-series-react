6b1f3f8852d80464b16c63b54feea3dfeb15b633
Paulo R. Araújo <pauloricardo722@gmail.com>
2018-12-09 19:28:44 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react'
+import api from './Api'
+class App extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      genres: [],
+      isLoading: false
+    }
+  }
+
+  componentDidMount(){
+    this.setState({ isLoading: true })
+    api.loadGenres()
+      .then((res)=>{
+        this.setState({
+          isLoading: false,
+          genres: res.data
+        })
+      })
+  }
+
+  renderGenreLink(genre) {
+    return (
+      <span>&nbsp;<a href=''>{genre}</a>&nbsp;</span>
+    )
+  }
+  render() {
+    return (
+      <div className="App">
+        <nav className="navbar navbar-default navbar-fixed-top" role="navigation">
+          <div className="container">
+            <div className="navbar-header page-scroll">
+              <a className="navbar-brand page-scroll" href="#page-top">
+                  <img src="images/logo.png" height="30" />
+              </a>
+            </div>
+
+            <div className="collapse navbar-collapse navbar-ex1-collapse">
+              <ul className="nav navbar-nav">
+                <li>
+                  <a href="">Menu item {this.state.count}</a>
+                </li>
+              </ul>
+            </div>
+
+          </div>
+        </nav>
+
+
+        <section id="intro" className="intro-section">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-12">
+                <h1><img src="images/logo.png" /></h1>
+                <p>Nunca mais esqueça uma série que você assistiu ou que alguém lhe indicou.</p>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section>
+          {
+            this.state.isLoading &&
+            <span>Aguarde, carregando...</span>
+          }
+          { 
+            !this.state.isLoading &&
+            <div>
+              Ver séries do genêro
+              {this.state.genres.map(this.renderGenreLink)}
+            </div>
+          }
+        </section>
+      </div>
+    );
+  }
+}
+
+export default App
